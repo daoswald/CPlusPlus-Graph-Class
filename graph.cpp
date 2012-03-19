f630b22f834e5758bde6b1d89fd28e5c15fef2b8
David Oswald <daoswald@gmail.com>
2012-03-18 19:23:12 -0700

    Added 'typename' keywords where necessary for g++ compilers.

--- graph.cpp
+++ graph.cpp
@@ -10,11 +10,11 @@
 #include <vector>                   // vector, vector<>::iterator
 #include <map>                      // map, pair
 
-#include <iostream>					// cerr.
+#include <iostream>                 // cerr.
 
 // Add vertices, one at a time.  Returns false if vertex already existed.
 // Throws under the impossible condition of an attempt to add to same
-// index twice.  
+// index twice.
 // Also throws if is_full() returns true, or if any memory allocation fails.
 template<class VertexT>
 bool Graph<VertexT>::add_vertex( VertexT v )
@@ -24,67 +24,73 @@ bool Graph<VertexT>::add_vertex( VertexT v )
     using std::pair;
     using std::runtime_error;
 
-	// is_full() *should* catch out of memory before we attempt to allocate,
-	// but we will also trap std::bad_alloc's later on just in case.
-	if( is_full() )
-		throw( runtime_error(
-			"add_vertex(): No room for more vertices."
-		) );  // This should be difficult but NOT impossible to reach.
+    // is_full() *should* catch out of memory before we attempt to allocate,
+    // but we will also trap std::bad_alloc's later on just in case.
+    if( is_full() )
+        throw( runtime_error(
+            "add_vertex(): No room for more vertices."
+        ) );  // This should be difficult but NOT impossible to reach.
 
     // Note: v_to_i.size() will return number of elements, which we can use
     // as the index number of the new element.
 
     // Add the vertex to our crossreference lookups.
-	// First, the vertex to index crossref.
-	pair< map< VertexT, std::vector<int>::size_type >::iterator,bool > v_to_i_ret;
-	try
-	{ 
-	    v_to_i_ret 
-			= v_to_i.insert( pair< VertexT, std::vector<int>::size_type >( v, v_to_i.size() ) );
-	}
-	catch( std::bad_alloc& ba )
-	{
-		std::cerr << "Bad Allocation caught adding to map: " 
-				  << ba.what() << std::endl;
-		throw( runtime_error(
-			"add_vertex(): Failed to allocate memory for new vertex."
-		) );
-	}
+    // First, the vertex to index crossref.
+    pair<
+        typename map<
+            VertexT,
+            typename std::vector<int>::size_type
+        >::iterator,
+        bool
+    > v_to_i_ret;
+    try
+    {
+        v_to_i_ret
+            = v_to_i.insert( pair< VertexT, std::vector<int>::size_type >( v, v_to_i.size() ) );
+    }
+    catch( std::bad_alloc& ba )
+    {
+        std::cerr << "Bad Allocation caught adding to map: "
+                  << ba.what() << std::endl;
+        throw( runtime_error(
+            "add_vertex(): Failed to allocate memory for new vertex."
+        ) );
+    }
 
-	if( !v_to_i_ret.second )
+    if( !v_to_i_ret.second )
         return false; // The vertex already existed.
-	
-	// Next, the index to vertex crossref.
-	try
-	{
-		i_to_v.push_back( v );
-	}
-	catch( std::bad_alloc& ba )
-	{
-		std::cerr << "Bad Allocation caught adding to index: " 
-				  << ba.what() << std::endl;
-		throw( runtime_error(
-			"add_vertex(): Failed to allocate memory for new vertex."
-		) );
-	}
+
+    // Next, the index to vertex crossref.
+    try
+    {
+        i_to_v.push_back( v );
+    }
+    catch( std::bad_alloc& ba )
+    {
+        std::cerr << "Bad Allocation caught adding to index: "
+                  << ba.what() << std::endl;
+        throw( runtime_error(
+            "add_vertex(): Failed to allocate memory for new vertex."
+        ) );
+    }
 
     // Add a column to each existing row of the matrix for this new vertex.
     vector<vector<int> >::iterator it;
     for( it=matrix.begin(); it != matrix.end(); ++it )
-	{
-		try
-		{
-	        it->push_back( NON_EDGE );
-		}
-		catch( std::bad_alloc& ba )
-		{
-			std::cerr << "Bad Allocation caught adding to matrix: " 
-					  << ba.what() << std::endl;
-			throw( runtime_error(
-				"add_vertex(): Failed to allocate memory for new vertex."
-			) );
-		}
-	}
+    {
+        try
+        {
+            it->push_back( NON_EDGE );
+        }
+        catch( std::bad_alloc& ba )
+        {
+            std::cerr << "Bad Allocation caught adding to matrix: "
+                      << ba.what() << std::endl;
+            throw( runtime_error(
+                "add_vertex(): Failed to allocate memory for new vertex."
+            ) );
+        }
+    }
 
     // Note: v_to_i.size() will now return the number of elements inclusive
     // of the new vertex, so we can use that as the number of columns for
@@ -103,27 +109,26 @@ bool Graph<VertexT>::add_vertex( VertexT v )
 template<class VertexT>
 int Graph<VertexT>::get_vertices( std::vector<VertexT>& vertices ) const
 {
-	using std::map;
-	using std::pair;
-	int size = num_vertices();
-	vector<VertexT>::const_iterator vit;
-	for( vit = i_to_v.begin(); vit != i_to_v.end(); ++vit )
-	{
-		vertices.push_back( *vit );
-	}
-	return size;
+    using std::vector;
+    int size = num_vertices();
+    typename vector<VertexT>::const_iterator vit;
+    for( vit = i_to_v.begin(); vit != i_to_v.end(); ++vit )
+    {
+        vertices.push_back( *vit );
+    }
+    return size;
 }
 
 // Returns a vertex count.
 template<class VertexT>
 int Graph<VertexT>::num_vertices() const
 {
-	using std::runtime_error;
-	if( i_to_v.size() != v_to_i.size() || v_to_i.size() != matrix.size() )
-		throw( runtime_error(
-			"Graph components out of sync: Detected in num_vertices()"
-		) ); // Should be impossible.  Just a sanity check.
-	return i_to_v.size();
+    using std::runtime_error;
+    if( i_to_v.size() != v_to_i.size() || v_to_i.size() != matrix.size() )
+        throw( runtime_error(
+            "Graph components out of sync: Detected in num_vertices()"
+        ) ); // Should be impossible.  Just a sanity check.
+    return i_to_v.size();
 }
 
 // Removes all vertices.
@@ -131,14 +136,14 @@ int Graph<VertexT>::num_vertices() const
 template<class VertexT>
 void Graph<VertexT>::make_empty()
 {
-	using std::runtime_error;
+    using std::runtime_error;
     v_to_i.clear();
     i_to_v.clear();
     matrix.clear();
-	if( ! v_to_i.empty() || ! i_to_v.empty() || ! matrix.empty() )
-		throw( runtime_error(
-			"make_empty(): One or more components of Graph failed to clear"
-		) );  // Impossible, but never hurts to be careful.
+    if( ! v_to_i.empty() || ! i_to_v.empty() || ! matrix.empty() )
+        throw( runtime_error(
+            "make_empty(): One or more components of Graph failed to clear"
+        ) );  // Impossible, but never hurts to be careful.
     return;
 }
 
@@ -146,14 +151,14 @@ void Graph<VertexT>::make_empty()
 template<class VertexT>
 bool Graph<VertexT>::is_empty() const
 {
-	using std::runtime_error;
-	if( 
-		i_to_v.empty() != v_to_i.empty()	||
-		v_to_i.empty() != matrix.empty() 
-	)
-		throw( runtime_error(
-			"Graph components out of sync: detected in is_empty()"
-		) );	// This should be impossible.  Just a sanity check.
+    using std::runtime_error;
+    if(
+        i_to_v.empty() != v_to_i.empty()    ||
+        v_to_i.empty() != matrix.empty()
+    )
+        throw( runtime_error(
+            "Graph components out of sync: detected in is_empty()"
+        ) );    // This should be impossible.  Just a sanity check.
     return matrix.empty(); // Returns true for empty, false otherwise.
 }
 
@@ -185,7 +190,11 @@ template<class VertexT>
 int Graph<VertexT>::index_is( VertexT v ) const
 {
     using std::map;
-    map<VertexT,std::vector<int>::size_type>::const_iterator idx_it = v_to_i.find( v );
+    typename map<
+        VertexT,
+        typename std::vector<int>::size_type
+    >::const_iterator idx_it
+        = v_to_i.find( v );
     if( idx_it == v_to_i.end() )
         return -1;  // Sentinal flag: Vertex doesn't exist!
     else
@@ -198,13 +207,13 @@ VertexT Graph<VertexT>::vertex_is( std::vector<int>::size_type i ) const
 {
     using std::map;
     using std::runtime_error;
-	if( i > i_to_v.size() - 1 )
-		throw( runtime_error(
-			"vertex_is() called on an out of range index: "
-			"No vertex by that index."
-		) );
-	else
-		return i_to_v[i];
+    if( i > i_to_v.size() - 1 )
+        throw( runtime_error(
+            "vertex_is() called on an out of range index: "
+            "No vertex by that index."
+        ) );
+    else
+        return i_to_v[i];
 }
 
 
@@ -219,8 +228,8 @@ bool Graph<VertexT>::add_edge( VertexT va, VertexT vb, int weight )
         return false;
     else {
         matrix[row][col] = weight;
-		// We implement non-directed as auto-two-way-direction.
-		if( !directed ) matrix[col][row] = weight;
+        // We implement non-directed as auto-two-way-direction.
+        if( !directed ) matrix[col][row] = weight;
         return true;
     }
 }
@@ -247,16 +256,16 @@ bool Graph<VertexT>::edge_exists( VertexT va, VertexT vb ) const
 template<class VertexT>
 void Graph<VertexT>::delete_edge( VertexT va, VertexT vb )
 {
-	using std::runtime_error;
+    using std::runtime_error;
     int row = index_is( va );
     int col = index_is( vb );
-	if( row < 0 || col < 0 )
-		throw( runtime_error(
-			"delete_edge(): Attempt to delete an edge with invalid vertex."
-		) );
+    if( row < 0 || col < 0 )
+        throw( runtime_error(
+            "delete_edge(): Attempt to delete an edge with invalid vertex."
+        ) );
     matrix[row][col] = NON_EDGE;
-	// non-directed implemented as automatic direct-back (2-way direction).
-	if( !directed ) matrix[col][row] = NON_EDGE;
+    // non-directed implemented as automatic direct-back (2-way direction).
+    if( !directed ) matrix[col][row] = NON_EDGE;
     return;
 }
 
@@ -266,12 +275,12 @@ void Graph<VertexT>::delete_edge( VertexT va, VertexT vb )
 template<class VertexT>
 int Graph<VertexT>::get_weight( VertexT va, VertexT vb ) const
 {
-	using std::runtime_error;
+    using std::runtime_error;
     int row = index_is( va );
     int col = index_is( vb );
     if( row < 0 || col < 0 )
         throw( runtime_error(
-			"get_weight(): Can't get a weight of edge with invalid vertex."
+            "get_weight(): Can't get a weight of edge with invalid vertex."
         ) );
     else
         return matrix[row][col];
@@ -282,24 +291,24 @@ int Graph<VertexT>::get_weight( VertexT va, VertexT vb ) const
 // Any non-zero column is an edge.
 template<class VertexT>
 void Graph<VertexT>::get_adjacent(
-    VertexT v, 
-	std::priority_queue< std::pair<VertexT,int>, 
-						 std::vector< std::pair<VertexT,int> >, 
-						 PairComparator< VertexT > >& pq
+    VertexT v,
+    std::priority_queue< std::pair<VertexT,int>,
+                         std::vector< std::pair<VertexT,int> >,
+                         PairComparator< VertexT > >& pq
 ) const
 {
-	using std::runtime_error;
-	using std::pair;
-	int r_ix = index_is( v );
-	if( r_ix < 0 )
-		throw( runtime_error(
-			"get_adjacent(): Invalid vertix."
-		) );
-	for( unsigned c_ix = 0; c_ix < matrix[r_ix].size(); c_ix++ )
-		if( int weight = matrix[r_ix][c_ix] )
-		{
-			VertexT vb = vertex_is( c_ix );
-			pq.push( pair<VertexT,int>( vb, weight ) );
-		}
-	return;
+    using std::runtime_error;
+    using std::pair;
+    int r_ix = index_is( v );
+    if( r_ix < 0 )
+        throw( runtime_error(
+            "get_adjacent(): Invalid vertix."
+        ) );
+    for( unsigned c_ix = 0; c_ix < matrix[r_ix].size(); c_ix++ )
+        if( int weight = matrix[r_ix][c_ix] )
+        {
+            VertexT vb = vertex_is( c_ix );
+            pq.push( pair<VertexT,int>( vb, weight ) );
+        }
+    return;
 }
--- graph.h
+++ graph.h
@@ -20,10 +20,10 @@
 
 
 // Some constants that will be useful:
-const bool DIRECTED		= true;
-const bool DIGRAPH		= true;
-const bool UNDIRECTED	= false;
-const bool NONDIRECTED	= false;
+const bool DIRECTED     = true;
+const bool DIGRAPH      = true;
+const bool UNDIRECTED   = false;
+const bool NONDIRECTED  = false;
 const bool NOT_DIRECTED = false;
 const bool NON_DIRECTED = false;
 
@@ -33,25 +33,25 @@ const int  NON_EDGE     = 0;
 // A comparator class for priority queues.
 template< typename VertexT >
 struct PairComparator {
-	bool operator()( 
-		const std::pair<VertexT,int>& p1, 
-		const std::pair<VertexT,int>& p2 
-	) const 
-    {  
-		if( p1.second > p2.second ) 
-			return true;
-		else
-			return false;
+    bool operator()(
+        const std::pair<VertexT,int>& p1,
+        const std::pair<VertexT,int>& p2
+    ) const
+    {
+        if( p1.second > p2.second )
+            return true;
+        else
+            return false;
     }
 };
 
 
-// Implement something similar to the following typedef within the calling 
-// code for Graph to facilitate cleaner syntax in using 
+// Implement something similar to the following typedef within the calling
+// code for Graph to facilitate cleaner syntax in using
 // Graph::get_adjacent().
-// typedef std::priority_queue< std::pair< string, int >, 
-//								std::vector< std::pair< string, int > >, 
-//								PairComparator< string > 
+// typedef std::priority_queue< std::pair< string, int >,
+//                              std::vector< std::pair< string, int > >,
+//                              PairComparator< string >
 // > charPQ;
 
 
@@ -63,27 +63,27 @@ class Graph {
     Graph( bool is_directed = true ) { directed = is_directed; return; }
     virtual ~Graph(){ return; }
     virtual bool add_vertex( VertexT V );
-	virtual int  get_vertices( std::vector<VertexT>& vertices ) const;
-	virtual int  num_vertices() const;
+    virtual int  get_vertices( std::vector<VertexT>& vertices ) const;
+    virtual int  num_vertices() const;
     virtual void make_empty();
     virtual bool is_empty()     const;
-    virtual bool is_full()      const;
-    virtual bool add_edge( VertexT va, VertexT vb, int weight = 1 );
+    virtual bool is_full ()     const;
+    virtual bool add_edge   ( VertexT va, VertexT vb, int weight = 1 );
     virtual bool edge_exists( VertexT va, VertexT vb ) const; // Assignment 2.
     virtual void delete_edge( VertexT va, VertexT vb );       // Assignment 2.
     virtual int  get_weight ( VertexT va, VertexT vb ) const; // Even better.
     virtual void get_adjacent(
-        VertexT v, 
-		std::priority_queue< std::pair<VertexT,int>, 
-			                 std::vector< std::pair<VertexT,int> 
-		>, 
-		PairComparator< VertexT > >& pq
+        VertexT v,
+        std::priority_queue< std::pair<VertexT,int>,
+                             std::vector< std::pair<VertexT,int>
+        >,
+        PairComparator< VertexT > >& pq
     ) const;
   private:
     virtual int     index_is ( VertexT  v ) const;
     virtual VertexT vertex_is( std::vector<int>::size_type i ) const;
 
-	bool directed;
+    bool directed;
     std::vector< std::vector< int > >             matrix; // Adjacency matrix.
     std::map<VertexT,std::vector<int>::size_type> v_to_i; // Verts to idx's.
     std::vector< VertexT >                        i_to_v; // idx's to Verts.
